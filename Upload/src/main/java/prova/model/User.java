a58267322630a795e46050d3e72e0fc9de597f3e
Tontoranelli <Tontoranelli@192.168.2.201>
2019-02-26 11:56:23 +0100

    User

--- Upload/src/main/java/prova/model/User.java
+++ Upload/src/main/java/prova/model/User.java
@@ -1,5 +1,55 @@
 package prova.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 public class User {
+	@Id
+	@GeneratedValue
+	long id;
+	@Email
+	private String email;
+	@NotNull
+	private String username;
+	@NotNull
+	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User() {
+		super();
+	}
 
 }
