c1c39d49991bb6b11a458fbcf2c1bfdec0c5e8ce
Tontoranelli <Tontoranelli@192.168.2.201>
2019-02-26 12:47:42 +0100

    change

--- Upload/src/main/java/prova/services/UserService.java
+++ Upload/src/main/java/prova/services/UserService.java
@@ -1,5 +1,8 @@
 package prova.services;
 
+import prova.model.User;
+
 public interface UserService {
 
+	public User findByUsername(String username);
 }
--- Upload/src/main/java/prova/services/UserServiceImpl.java
+++ Upload/src/main/java/prova/services/UserServiceImpl.java
@@ -1,5 +1,18 @@
 package prova.services;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import prova.model.User;
+import prova.repository.UserRepository;
+
+public class UserServiceImpl implements UserService{
+@Autowired
+UserRepository user_repo;
+
+	@Override
+	public User findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return user_repo.findByUsername(username);
+	}
 
 }
