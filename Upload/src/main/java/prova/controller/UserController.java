06fa72c9cd24f4a8c42339d5878ba4de47afd097
Alfonso De Pascale <Alfonso De Pascale@192.168.2.71>
2019-02-26 13:01:20 +0100

    push

--- Upload/src/main/java/prova/controller/UserController.java
+++ Upload/src/main/java/prova/controller/UserController.java
@@ -1,5 +1,39 @@
 package prova.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import prova.exception.BadRequest;
+import prova.model.User;
+import prova.services.UserService;
+import prova.services.auth.AuthService;
+
+@RestController
 public class UserController {
 
+	@Autowired
+	private UserService user_service;
+	
+	@Autowired
+	private AuthService auth_service;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<User> registraUser(@RequestBody User user) throws BadRequest {
+    	user.setPassword(encoder.encode(user.getPassword()));
+    	return user_service.registra(user);
+    }
+@RequestMapping(value="/login",method=RequestMethod.GET)
+ public UserDetails authenticate(@RequestBody User user)throws Exception{
+	return auth_service.authenticate(user);
+
+}
 }
--- Upload/src/main/java/prova/services/UserService.java
+++ Upload/src/main/java/prova/services/UserService.java
@@ -1,8 +1,13 @@
 package prova.services;
 
+import org.springframework.http.ResponseEntity;
+
+import prova.exception.BadRequest;
 import prova.model.User;
 
 public interface UserService {
 
 	public User findByUsername(String username);
+	
+	public ResponseEntity<User> registra(User user) throws BadRequest;
 }
