12529df87252652c0511fd864e9446fbd118ba62
Alfonso De Pascale <Alfonso De Pascale@192.168.2.71>
2019-02-27 16:28:16 +0100

    servizio logout

--- src/main/java/prova/controller/UserController.java
+++ src/main/java/prova/controller/UserController.java
@@ -1,9 +1,15 @@
 package prova.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,26 +25,32 @@ public class UserController {
 
 	@Autowired
 	private UserService user_service;
-
+	
 	@Autowired
 	private AuthService auth_service;
-
+	
 	@Autowired
 	private PasswordEncoder encoder;
-
+	
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public ResponseEntity<Users> registraUser(@RequestBody Users user) throws BadRequest {
-		user.setPassword(encoder.encode(user.getPassword()));
-		return user_service.registra(user);
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public UserDetails authenticate(@RequestBody Users user) throws Exception {
-		return auth_service.authenticate(user);
-
-	}
+    public ResponseEntity<Users> registraUser(@RequestBody Users user) throws BadRequest {
+    	user.setPassword(encoder.encode(user.getPassword()));
+    	return user_service.registra(user);
+    }
+@RequestMapping(value="/login",method=RequestMethod.POST)
+ public UserDetails authenticate(@RequestBody Users user)throws Exception{
+	return auth_service.authenticate(user);
+}
 @RequestMapping(value = "/stampa", method = RequestMethod.GET)
 public String stampa() throws BadRequest{
 	return user_service.stampa();
+}
+@RequestMapping(value="/logout", method = RequestMethod.GET)
+public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth != null){    
+        new SecurityContextLogoutHandler().logout(request, response, auth);
+    }
+    return "logout occurred";
 }
 }
